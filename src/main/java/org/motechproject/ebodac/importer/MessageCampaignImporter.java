24d171c3c2d3e7e7d93f682cafb536810a64b81b
Jakub Kondrat <jkondrat@soldevelo.com>
2015-04-10 15:00:52 +0200

    EBODAC-107 - Investigate and implement a way to automatically import tasks and campaigns

---
+++ src/main/java/org/motechproject/ebodac/importer/MessageCampaignImporter.java
@@ -0,0 +1,43 @@
+package org.motechproject.ebodac.importer;
+
+
+import org.eclipse.gemini.blueprint.service.importer.OsgiServiceLifecycleListener;
+import org.motechproject.messagecampaign.domain.campaign.CampaignRecord;
+import org.motechproject.messagecampaign.loader.CampaignJsonLoader;
+import org.motechproject.messagecampaign.service.MessageCampaignService;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class MessageCampaignImporter implements OsgiServiceLifecycleListener {
+
+    private MessageCampaignService messageCampaignService;
+
+    private CampaignJsonLoader campaignJsonLoader = new CampaignJsonLoader();
+
+    @Override
+    public void bind(Object o, Map map) throws Exception {
+        this.messageCampaignService = (MessageCampaignService) o;
+        importMessageCampaigns();
+    }
+
+    @Override
+    public void unbind(Object o, Map map) throws Exception {
+        this.messageCampaignService = null;
+    }
+
+    private void importMessageCampaigns() {
+        InputStream campaigns = getClass().getResourceAsStream("/message-campaign.json");
+        List<CampaignRecord> campaignRecords = campaignJsonLoader.loadCampaigns(campaigns);
+
+        for (CampaignRecord campaignRecord : campaignRecords) {
+            if (messageCampaignService.getCampaignRecord(campaignRecord.getName()) == null) {
+                messageCampaignService.saveCampaign(campaignRecord);
+            }
+        }
+    }
+
+}
---
+++ src/main/java/org/motechproject/ebodac/importer/TaskImporter.java
@@ -0,0 +1,68 @@
+package org.motechproject.ebodac.importer;
+
+import org.apache.commons.io.IOUtils;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.eclipse.gemini.blueprint.service.importer.OsgiServiceLifecycleListener;
+import org.motechproject.tasks.domain.Task;
+import org.motechproject.tasks.service.TaskService;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class TaskImporter implements OsgiServiceLifecycleListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskImporter.class);
+
+    private TaskService taskService;
+
+    @Override
+    public void bind(Object o, Map map) throws Exception {
+        this.taskService = (TaskService) o;
+        importTasks();
+    }
+
+    @Override
+    public void unbind(Object o, Map map) throws Exception {
+        this.taskService = null;
+    }
+
+    private void importTasks() {
+        Bundle bundle = FrameworkUtil.getBundle(getClass());
+        Enumeration<URL> urls = bundle.findEntries("tasks/", "*.json", false);
+
+        while (urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            try {
+                InputStream inputStream = url.openStream();
+                String json = IOUtils.toString(inputStream);
+
+                if (!taskExists(json)) {
+                    taskService.importTask(json);
+                }
+            } catch (IOException ioe) {
+                LOGGER.error("Couldn't read task file " + url.getPath());
+            }
+        }
+    }
+
+    private Boolean taskExists(String json) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode node = mapper.readTree(json);
+        Task task = mapper.readValue(node, Task.class);
+
+        List<Task> existingTasks = taskService.findTasksByName(task.getName());
+        return existingTasks.size() > 0;
+    }
+
+}
