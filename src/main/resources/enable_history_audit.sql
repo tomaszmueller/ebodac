1fa10865fb69ee3989b2a848907ad55b222439bc
Jakub Kondrat <jkondrat@soldevelo.com>
2015-04-28 14:17:02 +0200

    EBODAC-145: Enable History Audit

---
+++ src/main/resources/enable_history_audit.sql
@@ -0,0 +1,13 @@
+-- enables history audit for CSD ---
+
+UPDATE Tracking 
+SET 
+    recordHistory = 1,
+    modifiedByUser = 1
+WHERE
+    entity_id_OID IN (SELECT 
+            id
+        FROM
+            Entity
+        WHERE
+            className LIKE 'org.motechproject.csd.%');
