4a9d58b65d3e4911bd8cf6c4907e8d8418094952
Pawel Muchowski <pmuchowski@soldevelo.com>
2015-05-06 16:07:50 +0200

    Embed Subject DSUI to Ebodac tab

---
+++ src/main/java/org/motechproject/ebodac/constants/EbodacConstants.java
@@ -0,0 +1,9 @@
+package org.motechproject.ebodac.constants;
+
+public final class EbodacConstants {
+
+    public static final String UI_CONFIG = "custom-ui.js";
+
+    private EbodacConstants() {
+    }
+}
