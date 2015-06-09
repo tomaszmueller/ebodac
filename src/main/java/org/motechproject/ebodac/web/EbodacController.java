fe2a869919e21316fb576a4c07317a43f782ed38
Krzysztof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2015-06-09 14:50:56 +0200

    added ws for running zetes job

--- src/main/java/org/motechproject/ebodac/web/EbodacController.java
+++ src/main/java/org/motechproject/ebodac/web/EbodacController.java
@@ -2,16 +2,20 @@ package org.motechproject.ebodac.web;
 
 import org.apache.commons.io.IOUtils;
 import org.motechproject.ebodac.constants.EbodacConstants;
+import org.motechproject.ebodac.domain.Config;
+import org.motechproject.ebodac.service.ConfigService;
+import org.motechproject.ebodac.service.EbodacService;
+import org.motechproject.mds.util.Constants;
 import org.motechproject.server.config.SettingsFacade;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.motechproject.ebodac.service.EbodacService;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.io.IOException;
@@ -29,12 +33,29 @@ public class EbodacController {
     @Qualifier("ebodacSettings")
     private SettingsFacade settingsFacade;
 
+    @Autowired
+    private ConfigService configService;
+
     private static final String OK = "OK";
 
     @RequestMapping("/web-api/status")
     @ResponseBody
     public String status() {
         return OK;
+    }
+
+    @RequestMapping(value = "/runJob", method = RequestMethod.POST)
+    @PreAuthorize(Constants.Roles.HAS_DATA_ACCESS)
+    @ResponseBody
+    public ResponseEntity<String> runZetesJob() {
+        Config config = configService.getConfig();
+
+        try {
+            ebodacService.sendUpdatedSubjects(config.getZetesUrl(), config.getZetesUsername(), config.getZetesPassword());
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @ResponseStatus(HttpStatus.OK)
