38d42e178e439f766018cb4fb0ffbbe65600d89b
Jakub Kondrat <jkondrat@soldevelo.com>
2015-06-12 19:14:13 +0200

    EBODAC-234: Add maps for language and message name

---
+++ src/main/java/org/motechproject/ebodac/repository/VotoLanguageDataService.java
@@ -0,0 +1,19 @@
+package org.motechproject.ebodac.repository;
+
+import org.motechproject.ebodac.domain.Language;
+import org.motechproject.ebodac.domain.VotoLanguage;
+import org.motechproject.mds.annotations.Lookup;
+import org.motechproject.mds.annotations.LookupField;
+import org.motechproject.mds.service.MotechDataService;
+
+/**
+ * Interface for repository that persists simple records and allows CRUD.
+ * MotechDataService base class will provide the implementation of this class as well
+ * as methods for adding, deleting, saving and finding all instances.  In this class we
+ * define and custom lookups we may need.
+ */
+public interface VotoLanguageDataService extends MotechDataService<VotoLanguage> {
+
+    @Lookup
+    VotoLanguage findVotoLanguageByLanguage(@LookupField(name = "language") Language language);
+}
