bea4052ae7fe3a7122595a06f9f8f58dbf34ac65
dwieczerzak <dwieczerzak@soldevelo.com>
2015-07-09 12:42:06 +0200

    EBODAC-332: DEV - Write CRUD tests for Subject service

--- src/test/java/org/motechproject/ebodac/osgi/SubjectServiceIT.java
+++ src/test/java/org/motechproject/ebodac/osgi/SubjectServiceIT.java
@@ -1,11 +1,18 @@
 package org.motechproject.ebodac.osgi;
 
-import org.junit.Ignore;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.motechproject.ebodac.constants.EbodacConstants;
+import org.motechproject.ebodac.domain.Gender;
 import org.motechproject.ebodac.domain.Language;
 import org.motechproject.ebodac.domain.Subject;
+import org.motechproject.ebodac.repository.SubjectDataService;
+import org.motechproject.ebodac.repository.VisitDataService;
 import org.motechproject.ebodac.service.SubjectService;
 import org.motechproject.testing.osgi.BasePaxIT;
 import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
@@ -18,12 +25,9 @@ import javax.inject.Inject;
 import java.util.List;
 
 import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static junit.framework.Assert.assertNotNull;
 
-/**
- * Verify that SubjectService present, functional.
- */
+
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerSuite.class)
 @ExamFactory(MotechNativeTestContainerFactory.class)
@@ -32,22 +36,275 @@ public class SubjectServiceIT extends BasePaxIT {
     @Inject
     private SubjectService subjectService;
 
-    @Ignore
+    @Inject
+    private SubjectDataService subjectDataService;
+
+    @Inject
+    private VisitDataService visitDataService;
+
+    private Subject firstSubject;
+
+    private Subject secondSubject;
+
+    @Before
+    public void cleanBefore() {
+        visitDataService.deleteAll();
+        subjectDataService.deleteAll();
+        resetSubjects();
+    }
+
+    @After
+    public void cleanAfter() {
+        visitDataService.deleteAll();
+        subjectDataService.deleteAll();
+    }
+
+    private void resetSubjects() {
+        firstSubject = new Subject("1000000161", "Michal", "Abacki", "Cabacki",
+                "729402018364", "address", Language.English, "community", "B05-SL10001");
+
+        secondSubject = new Subject("1000000162", "Rafal", "Dabacki", "Ebacki",
+                "44443333222", "address", Language.Susu, "community", "B05-SL10001");
+    }
+
     @Test
-    public void testSubjectService() throws Exception {
-        Subject testRecord = new Subject("123", "test 1st name", "test household name",
-                "ASD-22", "entity-facility-id", "Sesame Street", Language.English, "Jason Bourne",
-                EbodacConstants.SITE_ID_FOR_STAGE_I);
-        subjectService.createOrUpdateForZetes(testRecord);
+    public void shouldCreateOrUpdateForZetes() {
+        assertEquals(0, subjectDataService.retrieveAll().size());
+        assertEquals(0, visitDataService.retrieveAll().size());
 
-        List<Subject> record = subjectService.findSubjectByName(testRecord.getName());
-        assertEquals(testRecord, record.get(0));
+        subjectService.create(firstSubject);
+        List<Subject> subjects = subjectDataService.retrieveAll();
+        assertEquals(1, subjects.size());
 
-        List<Subject> records = subjectService.getAll();
-        assertTrue(records.contains(testRecord));
+        checkZetesFields(firstSubject, subjects.get(0));
+        checkRaveFields(firstSubject, subjects.get(0));
 
-        subjectService.delete(testRecord);
-        record = subjectService.findSubjectByName(testRecord.getName());
-        assertNull(record);
+        firstSubject.setAddress("newAddress");
+        firstSubject.setLanguage(Language.Limba);
+        firstSubject.setPhoneNumber("600700800");
+
+        subjectService.createOrUpdateForZetes(firstSubject); // should update Subject
+        subjects = subjectDataService.retrieveAll();
+        assertEquals(1, subjects.size());
+
+        checkZetesFields(firstSubject, subjects.get(0));
+        checkRaveFields(firstSubject, subjects.get(0));
+
+        subjectService.createOrUpdateForZetes(secondSubject); // should create new Subject
+        subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        Subject subjectFromDataService = subjectDataService.findSubjectBySubjectId("1000000162");
+        assertNotNull(subjectFromDataService);
+
+        checkZetesFields(secondSubject, subjectFromDataService);
+        checkRaveFields(secondSubject, subjectFromDataService);
+
+        secondSubject.setName("Jedrzej");
+        secondSubject.setHouseholdName("Gabacki");
+        secondSubject.setHeadOfHousehold("Ibacki");
+        secondSubject.setCommunity("newCommunity");
+
+        subjectDataService.update(secondSubject);
+        subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        subjectFromDataService = subjectDataService.findSubjectBySubjectId("1000000162");
+        assertNotNull(subjectFromDataService);
+
+        checkZetesFields(secondSubject, subjectFromDataService);
+        checkRaveFields(secondSubject, subjectFromDataService);
+    }
+
+    @Test
+    public void shouldCreateOrUpdateForRave() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern(EbodacConstants.REPORT_DATE_FORMAT);
+        assertEquals(0, subjectDataService.retrieveAll().size());
+        assertEquals(0, visitDataService.retrieveAll().size());
+
+        subjectService.create(firstSubject);
+        List<Subject> subjects = subjectDataService.retrieveAll();
+        assertEquals(1, subjects.size());
+
+        checkZetesFields(firstSubject, subjects.get(0));
+
+        firstSubject.setDateOfDisconStd(DateTime.parse("2014-10-17", formatter));
+        firstSubject.setGender(Gender.Male);
+        firstSubject.setStageId((long) 1);
+
+        subjectService.createOrUpdateForRave(firstSubject); // should update Subject
+        subjects = subjectDataService.retrieveAll();
+        assertEquals(1, subjects.size());
+
+        checkZetesFields(firstSubject, subjects.get(0));
+        checkRaveFields(firstSubject, subjects.get(0));
+
+        subjectService.createOrUpdateForRave(secondSubject); // should create new Subject
+        subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        Subject subjectFromDataService = subjectDataService.findSubjectBySubjectId("1000000162");
+        assertNotNull(subjectFromDataService);
+
+        checkZetesFields(secondSubject, subjectFromDataService);
+        checkRaveFields(secondSubject, subjectFromDataService);
+
+        secondSubject.setDateOfBirth(DateTime.parse("1967-05-02", formatter));
+        secondSubject.setPrimerVaccinationDate(DateTime.parse("2014-10-12", formatter));
+        secondSubject.setBoosterVaccinationDate(DateTime.parse("2014-10-17", formatter));
+
+        subjectDataService.update(secondSubject);
+        subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        subjectFromDataService = subjectDataService.findSubjectBySubjectId("1000000162");
+        assertNotNull(subjectFromDataService);
+
+        checkZetesFields(secondSubject, subjectFromDataService);
+        checkRaveFields(secondSubject, subjectFromDataService);
+    }
+
+    @Test
+    public void shouldFindSubjectBySubjectId() {
+        assertEquals(0, subjectDataService.retrieveAll().size());
+        assertEquals(0, visitDataService.retrieveAll().size());
+
+        subjectService.create(firstSubject);
+        subjectService.create(secondSubject);
+
+        List<Subject> subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        Subject subject = subjectService.findSubjectBySubjectId("1000000161");
+        assertNotNull(subject);
+        assertEquals("1000000161", subject.getSubjectId());
+    }
+
+    @Test
+    public void shouldFindModifiedSubjects() {
+        assertEquals(0, subjectDataService.retrieveAll().size());
+        assertEquals(0, visitDataService.retrieveAll().size());
+
+        subjectService.create(firstSubject);
+        subjectService.create(secondSubject);
+
+        List<Subject> subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        firstSubject.setChanged(true);
+
+        subjectService.update(firstSubject);
+
+        subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        subjects = subjectService.findModifiedSubjects();
+        assertEquals(1, subjects.size());
+
+        assertEquals("1000000161", subjects.get(0).getSubjectId());
+    }
+
+    @Test
+    public void shouldFindSubjectByName() {
+        assertEquals(0, subjectDataService.retrieveAll().size());
+        assertEquals(0, visitDataService.retrieveAll().size());
+
+        subjectService.create(firstSubject);
+        subjectService.create(secondSubject);
+
+        List<Subject> subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        subjects = subjectService.findSubjectByName("Michal");
+        assertEquals(1, subjects.size());
+
+        assertEquals("Michal", subjects.get(0).getName());
+    }
+
+    @Test
+    public void shouldFindSubjectsBoosterVaccinatedAtDay() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern(EbodacConstants.REPORT_DATE_FORMAT);
+        assertEquals(0, subjectDataService.retrieveAll().size());
+        assertEquals(0, visitDataService.retrieveAll().size());
+
+        firstSubject.setBoosterVaccinationDate(DateTime.parse("2015-02-28", formatter));
+        secondSubject.setBoosterVaccinationDate(DateTime.parse("2015-03-18", formatter));
+
+        subjectService.create(firstSubject);
+        subjectService.create(secondSubject);
+
+        List<Subject> subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        subjects = subjectService.findSubjectsBoosterVaccinatedAtDay(DateTime.parse("2015-02-28", formatter));
+        assertEquals(1, subjects.size());
+
+        assertEquals(DateTime.parse("2015-02-28", formatter), subjects.get(0).getBoosterVaccinationDate());
+    }
+
+    @Test
+    public void shouldFindSubjectsPrimerVaccinatedAtDay() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern(EbodacConstants.REPORT_DATE_FORMAT);
+        assertEquals(0, subjectDataService.retrieveAll().size());
+        assertEquals(0, visitDataService.retrieveAll().size());
+
+        firstSubject.setPrimerVaccinationDate(DateTime.parse("2015-02-28", formatter));
+        secondSubject.setPrimerVaccinationDate(DateTime.parse("2015-03-18", formatter));
+
+        subjectService.create(firstSubject);
+        subjectService.create(secondSubject);
+
+        List<Subject> subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        subjects = subjectService.findSubjectsPrimerVaccinatedAtDay(DateTime.parse("2015-02-28", formatter));
+        assertEquals(1, subjects.size());
+
+        assertEquals(DateTime.parse("2015-02-28", formatter), subjects.get(0).getPrimerVaccinationDate());
+    }
+
+    @Test
+    public void shouldFindOldestPrimerVaccinationDate() {
+        DateTimeFormatter formatter = DateTimeFormat.forPattern(EbodacConstants.REPORT_DATE_FORMAT);
+        assertEquals(0, subjectDataService.retrieveAll().size());
+        assertEquals(0, visitDataService.retrieveAll().size());
+
+        firstSubject.setPrimerVaccinationDate(DateTime.parse("2015-02-28", formatter));
+        secondSubject.setPrimerVaccinationDate(DateTime.parse("2015-03-18", formatter));
+
+        subjectService.create(firstSubject);
+        subjectService.create(secondSubject);
+
+        List<Subject> subjects = subjectDataService.retrieveAll();
+        assertEquals(2, subjects.size());
+
+        DateTime oldest = subjectService.findOldestPrimerVaccinationDate();
+        assertEquals(DateTime.parse("2015-02-28", formatter), oldest);
+    }
+
+    private void checkZetesFields(Subject expected, Subject actual) {
+        assertEquals(expected.getSubjectId(), actual.getSubjectId());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getHouseholdName(), actual.getHouseholdName());
+        assertEquals(expected.getHeadOfHousehold(), actual.getHeadOfHousehold());
+        assertEquals(expected.getAddress(), actual.getAddress());
+        assertEquals(expected.getLanguageCode(), actual.getLanguageCode());
+        assertEquals(expected.getCommunity(),        actual.getCommunity());
+        assertEquals(expected.getSiteId(), actual.getSiteId());
+    }
+
+    private void checkRaveFields(Subject expected, Subject actual) {
+        assertEquals(expected.getSiteId(), actual.getSiteId());
+        assertEquals(expected.getSubjectId(), actual.getSubjectId());
+        assertEquals(expected.getGender(), actual.getGender());
+        assertEquals(expected.getStageId(), actual.getStageId());
+        assertEquals(expected.getDateOfBirth(), actual.getDateOfBirth());
+        assertEquals(expected.getPrimerVaccinationDate(), actual.getPrimerVaccinationDate());
+        assertEquals(expected.getBoosterVaccinationDate(), actual.getBoosterVaccinationDate());
+        assertEquals(expected.getDateOfDisconVac(), actual.getDateOfDisconVac());
+        assertEquals(expected.getDateOfDisconStd(), actual.getDateOfDisconStd());
     }
 }
