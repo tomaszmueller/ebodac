5a2624d2da92d6768b0dd015dc983f8021e84c2a
Tomasz Mueller <tmueller@soldevelo.com>
2015-10-15 15:32:23 +0200

    EBODAC-526 "QA - Motech Have access to all the modules Test"

---
+++ src/test/java/org/motechproject/ebodac/uitest/test/MotechAccessToAllModuelsUiTest.java
@@ -0,0 +1,53 @@
+package org.motechproject.ebodac.uitest.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.motech.page.LoginPage;
+import org.motech.test.TestBase;
+import org.motechproject.ebodac.uitest.helper.TestParticipant;
+import org.motechproject.ebodac.uitest.helper.UITestHttpClientHelper;
+import org.motechproject.ebodac.uitest.page.HomePage;
+
+import static junit.framework.Assert.assertTrue;
+
+public class MotechAccessToAllModuelsUiTest extends TestBase {
+
+    private LoginPage loginPage;
+    private HomePage homePage;
+    private String user;
+    private String password;
+    private UITestHttpClientHelper httpClientHelper;
+    private String url;
+
+    @Before
+    public void setUp() {
+        user = properties.getUserName();
+        password = properties.getPassword();
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+        url = properties.getWebAppUrl();
+        if(url.contains("localhost")) {
+            httpClientHelper = new UITestHttpClientHelper(url);
+            httpClientHelper.addParticipant(new TestParticipant());
+        }
+        if(homePage.expectedUrlPath() != currentPage().urlPath()) {
+            loginPage.login(user, password);
+        }
+    }
+
+    @Test
+    public void motechAccessToAllModulesUiTest() throws Exception {
+        assertTrue(homePage.isDataServicesModulePresent());
+        assertTrue(homePage.isEBODACModulePresent());
+        assertTrue(homePage.isEmailModulePresent());
+        assertTrue(homePage.isMessageCampaignModulePresent());
+        assertTrue(homePage.isIVRModulePresent());
+        assertTrue(homePage.isSMSModulePresent());
+        assertTrue(homePage.isSchedulerModulePresent());
+        assertTrue(homePage.isTasksModulePresent());
+    }
+
+    public void tearDown() throws Exception {
+        loginPage.logOut();
+    }
+}
