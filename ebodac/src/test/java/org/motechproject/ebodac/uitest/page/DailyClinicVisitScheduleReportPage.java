7a0990f91e937cbecc1dae15f56d9e2002bf48cd
Tomasz Mueller <tmueller@soldevelo.com>
2015-12-29 11:53:26 +0100

    EBODAC-805 "QA - Reports - Daily Clinic Visit Schedule Report"

---
+++ ebodac/src/test/java/org/motechproject/ebodac/uitest/page/DailyClinicVisitScheduleReportPage.java
@@ -0,0 +1,32 @@
+package org.motechproject.ebodac.uitest.page;
+
+import org.motech.page.AbstractBasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class DailyClinicVisitScheduleReportPage extends AbstractBasePage {
+
+    public static final String URL_PATH = "/home#/mds/dataBrowser";
+    static final By RECORD = By.xpath("//table[@id='reportTable']/tbody/tr[2]");
+
+    public DailyClinicVisitScheduleReportPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isReportEmpty() {
+        try {
+            if (findElement(RECORD) != null) {
+                return false;
+            }
+            return true;
+        } catch(Exception e) {
+            return true;
+        }
+    }
+
+    @Override
+    public String expectedUrlPath() {
+        return URL_ROOT + URL_PATH;
+    }
+}
