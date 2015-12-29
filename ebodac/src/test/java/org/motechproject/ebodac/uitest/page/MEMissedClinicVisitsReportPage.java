763165aaccb10053dd7cdbd6911d046f1bc48a72
Tomasz Mueller <tmueller@soldevelo.com>
2015-12-29 15:23:39 +0100

    EBODAC-809 "QA - Reports - M&E Missed Clinic Visits Reports"

---
+++ ebodac/src/test/java/org/motechproject/ebodac/uitest/page/MEMissedClinicVisitsReportPage.java
@@ -0,0 +1,32 @@
+package org.motechproject.ebodac.uitest.page;
+
+import org.motech.page.AbstractBasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+public class MEMissedClinicVisitsReportPage extends AbstractBasePage {
+
+    public static final String URL_PATH = "/home#/mds/dataBrowser";
+    static final By RECORD = By.xpath("//table[@id='reportTable']/tbody/tr[2]");
+
+    public MEMissedClinicVisitsReportPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isReportEmpty() {
+        try {
+            if (findElement(RECORD) != null) {
+                return false;
+            }
+            return true;
+        } catch(Exception e) {
+            return true;
+        }
+    }
+
+    @Override
+    public String expectedUrlPath() {
+        return URL_ROOT + URL_PATH;
+    }
+}
---
+++ ebodac/src/test/java/org/motechproject/ebodac/uitest/test/MEMissedClinicVisitsReportUiTest.java
@@ -0,0 +1,52 @@
+package org.motechproject.ebodac.uitest.test;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.motech.page.LoginPage;
+import org.motech.test.TestBase;
+import org.motechproject.ebodac.uitest.page.MEMissedClinicVisitsReportPage;
+import org.motechproject.ebodac.uitest.page.EBODACPage;
+import org.motechproject.ebodac.uitest.page.HomePage;
+import org.motechproject.ebodac.uitest.page.ReportPage;
+
+import static org.junit.Assert.assertFalse;
+
+
+public class MEMissedClinicVisitsReportUiTest extends TestBase {
+
+    private LoginPage loginPage;
+    private HomePage homePage;
+    private EBODACPage ebodacPage;
+    private ReportPage reportPage;
+    private MEMissedClinicVisitsReportPage mEMissedClinicVisitsReportPage;
+    private String user;
+    private String password;
+
+    @Before
+    public void setUp() {
+        user = properties.getUserName();
+        password = properties.getPassword();
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+        ebodacPage = new EBODACPage(driver);
+        reportPage = new ReportPage(driver);
+        mEMissedClinicVisitsReportPage = new MEMissedClinicVisitsReportPage(driver);
+        if(homePage.expectedUrlPath() != currentPage().urlPath()) {
+            loginPage.login(user, password);
+        }
+    }
+
+    @Test
+    public void mEMissedClinicVisitsReportTest() throws InterruptedException {
+        homePage.openEBODACModule();
+        ebodacPage.gotoReports();
+        reportPage.showMEMissedClinicVisitsReport();
+        assertFalse(mEMissedClinicVisitsReportPage.isReportEmpty());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        loginPage.logOut();
+    }
+}
