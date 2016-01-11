88348a4bdd116b1c8c31db829c11c5f6000ed551
Pawel Muchowski <pmuchowski@soldevelo.com>
2016-01-11 16:21:54 +0100

    EBODAC-790: DEV - TASK - Booking Application - (Screen 6) - Reorder the Advance Settings

--- booking-app/src/main/resources/custom-ui.js
+++ booking-app/src/main/resources/custom-ui.js
@@ -8,6 +8,30 @@ $scope.showViewTrashButton = false;
 $scope.showFiltersButton = false;
 $scope.showDeleteInstanceButton = false;
 
+$scope.showAdvanced = false;
+$scope.advancedButtonIndex = 6;
+
+$scope.showOrHideAdvanced = function() {
+    var i, lastFieldIndex = 16;
+    $scope.showAdvanced = !$scope.showAdvanced;
+
+    for (i = $scope.advancedButtonIndex + 1; i <= lastFieldIndex; i += 1) {
+        if ($scope.showAdvanced) {
+            $scope.fields[i].nonDisplayable = false;
+        } else {
+            $scope.fields[i].nonDisplayable = true;
+        }
+    }
+};
+
+$scope.getAdvancedButtonLabel = function() {
+    if ($scope.showAdvanced) {
+        return $scope.msg('bookingApp.hideAdvanced');
+    } else {
+        return $scope.msg('bookingApp.showAdvanced');
+    }
+};
+
 $scope.editInstance = function(id, module, entityName) {
     blockUI();
     $scope.setHiddenFilters();
@@ -22,15 +46,41 @@ $scope.editInstance = function(id, module, entityName) {
             $scope.currentRecord = data;
             $scope.fields = data.fields;
             if (entityName === "Clinic") {
-                angular.forEach ($scope.fields, function(field) {
-                    if (field.name === "location" || field.name === "site") {
-                        field.nonEditable = true;
+                var i, showAdvancedButton = {
+                    'name': 'showAdvanced',
+                    'displayName': '',
+                    'tooltip': '',
+                    'value': '',
+                    'type': {
+                        'displayName': 'mds.field.string'
+                    },
+                    'required': false,
+                    'nonEditable': false,
+                    'nonDisplayable': false
+                };
+
+                $scope.showAdvanced = false;
+                $scope.fields.splice($scope.advancedButtonIndex, 0, showAdvancedButton);
+
+                for (i = 0; i < $scope.fields.length; i += 1) {
+                    if ($scope.fields[i].name === "location" || $scope.fields[i].name === "siteId") {
+                        $scope.fields[i].nonEditable = true;
                     }
-                });
+                    if (i > $scope.advancedButtonIndex) {
+                        $scope.fields[i].nonDisplayable = true;
+                    }
+                }
             }
 
             unblockUI();
         }, angularHandler('mds.error', 'mds.error.cannotUpdateInstance'));
+};
+
+$scope.addEntityInstanceDefault = $scope.addEntityInstance;
+
+$scope.addEntityInstance = function () {
+    $scope.fields.splice($scope.advancedButtonIndex, 1);
+    $scope.addEntityInstanceDefault();
 };
 
 $scope.showLookupDialog = function() {
@@ -39,4 +89,14 @@ $scope.showLookupDialog = function() {
     'left': ($("#lookupDialogButton").offset().left - $("#main-content").offset().left) - 15})
     .toggle();
     $("div.arrow").css({'left': 50});
+};
+
+$scope.loadEditValueFormDefault = $scope.loadEditValueForm;
+
+$scope.loadEditValueForm = function (field) {
+    if (field.name === 'showAdvanced') {
+        return '../booking-app/resources/partials/widgets/field-show-advanced.html';
+    }
+
+    return $scope.loadEditValueFormDefault(field);
 };
