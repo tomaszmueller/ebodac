4502bdeba4f0dbd5225c9b06720a0a5b2f70f62b
Pawel Muchowski <pmuchowski@soldevelo.com>
2016-02-09 17:03:15 +0100

    Changed Motech version to 0.27

--- booking-app/src/main/java/org/motechproject/bookingapp/listener/BookingAppLifecycleListener.java
+++ booking-app/src/main/java/org/motechproject/bookingapp/listener/BookingAppLifecycleListener.java
@@ -4,7 +4,7 @@ package org.motechproject.bookingapp.listener;
 import org.motechproject.bookingapp.domain.Clinic;
 import org.motechproject.ebodac.domain.Visit;
 import org.motechproject.mds.annotations.InstanceLifecycleListener;
-import org.motechproject.mds.domain.InstanceLifecycleListenerType;
+import org.motechproject.mds.annotations.InstanceLifecycleListenerType;
 
 public interface BookingAppLifecycleListener {
 
