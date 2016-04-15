8ada358a8d3ea97aa92b3f46847adc916cf8edef
pmuchowski <pmuchowski@soldevelo.com>
2016-04-15 15:33:21 +0200

    EBODAC-954: DEV - TASK - Maintenance - Remove SQLs from Java Classes (#576)

--- ebodac/src/main/java/org/motechproject/ebodac/service/StatisticService.java
+++ ebodac/src/main/java/org/motechproject/ebodac/service/StatisticService.java
@@ -5,13 +5,14 @@ import org.motechproject.commons.api.Range;
 import org.motechproject.ebodac.domain.IvrEngagementStatistic;
 import org.motechproject.ebodac.domain.IvrAndSmsStatistic;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface StatisticService {
 
-    List<IvrAndSmsStatistic> getStatisticForIvr(Range<DateTime> dateRange);
+    List<IvrAndSmsStatistic> getStatisticForIvr(Range<DateTime> dateRange) throws IOException;
 
-    List<IvrAndSmsStatistic> getStatisticForSms(Range<DateTime> dateRange);
+    List<IvrAndSmsStatistic> getStatisticForSms(Range<DateTime> dateRange) throws IOException;
 
-    List<IvrEngagementStatistic> getIvrEngagementStatistic();
+    List<IvrEngagementStatistic> getIvrEngagementStatistic() throws IOException;
 }
--- ebodac/src/main/resources/enable_history_audit_for_message_campaign.sql
+++
@@ -1,7 +0,0 @@
--- enables history audit for Message Campaign ---
-
-UPDATE Tracking
-INNER JOIN Entity
-ON Tracking.entity_id_OID = Entity.id
-SET Tracking.recordHistory = 1, Tracking.modifiedByUser = 1
-WHERE Entity.name="CampaignRecord" or Entity.name="CampaignMessageRecord";
