9eaa9bae335e096205e985427715229dff3d6681
tomaszmueller <tmueller@soldevelo.com>
2016-06-01 10:34:24 +0200

    fix for failing build (#617)

--- ebodac/src/test/java/org/motechproject/ebodac/uitest/page/EnrollmentPage.java
+++ ebodac/src/test/java/org/motechproject/ebodac/uitest/page/EnrollmentPage.java
@@ -7,13 +7,13 @@ import org.openqa.selenium.WebElement;
 
 public class EnrollmentPage extends AbstractBasePage {
     public static final String URL_PATH = "home#/ebodac/enrollment";
-    static final By ACTION = By.xpath("(//button[@type='button'])[3]");
+    static final By ACTION = By.xpath("//table[@id='enrollmentTable']/tbody/tr[2]/td[6]/button");
     static final By POPUP_OK = By.id("popup_ok");
     static final By POPUP_CONTENT = By.id("popup_content");
     static final int SLEEP_500 = 500;
     static final By ENROLLMENT_RECORD = By.xpath("//table[@id='enrollmentTable']/tbody/tr[2]");
     static final By ENROLLMENT_ADVANCED = By.id("enrollmentAdvanced");
-    static final int LAST_ENROLL = 3;
+    static final int LAST_ENROLL = 2;
     private int lastEnroll;
     static final int SMALL_TIMEOUT = 500;
     static final int BIG_TIMEOUT = 2000;
@@ -33,12 +33,12 @@ public class EnrollmentPage extends AbstractBasePage {
 
     public void clickAction() throws InterruptedException {
         Thread.sleep(SLEEP_500);
-        clickOn(ACTION);
+        clickWhenVisible(ACTION);
     }
 
     public void clickOK() throws InterruptedException {
         Thread.sleep(SMALL_TIMEOUT);
-        clickOn(POPUP_OK);
+        clickWhenVisible(POPUP_OK);
     }
     public boolean error() {
         try {
@@ -75,7 +75,7 @@ public class EnrollmentPage extends AbstractBasePage {
         while (error());
     }
     public void actionSecond() {
-        WebElement action = findElement(By.xpath("(//button[@type='button'])[" + lastEnroll + "]"));
+        WebElement action = findElement(By.xpath("//table[@id='enrollmentTable']/tbody/tr[" + lastEnroll + "]/td[6]/button"));
         action.click();
     }
 
