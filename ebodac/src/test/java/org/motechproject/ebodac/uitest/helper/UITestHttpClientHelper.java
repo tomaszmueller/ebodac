cb9538501d65f0484b608fb6264eeb2bf21a4d2d
rubenmartin <rmartinramos@soldevelo.com>
2016-08-19 10:46:41 +0200

    EBODAC-511 - fixed issue with GetVisitsDataFromRaveuiTest (19-08-2106)

--- ebodac/src/test/java/org/motechproject/ebodac/uitest/helper/UITestHttpClientHelper.java
+++ ebodac/src/test/java/org/motechproject/ebodac/uitest/helper/UITestHttpClientHelper.java
@@ -1,18 +1,115 @@
 package org.motechproject.ebodac.uitest.helper;
 
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 import org.motechproject.ebodac.client.EbodacHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UITestHttpClientHelper extends EbodacHttpClient {
-    private static final String URL_IMPORT_CSV = "/module/ebodac/web-api/import-csv";
-    private static final String URL_FETCH_CSV = "/module/ebodac/web-api/fetch-csv";
-    private static final String URL_PARTICIPANT_REGISTRATION = "/module/ebodac/registration/submit";
-    private static final Logger LOGGER = LoggerFactory.getLogger(UITestHttpClientHelper.class);
+
+    public static final String PARTICIPANT_ID = "participantId";
+    public static final String DATE_OF_BIRTH = "dateOfBirth";
+    public static final String GENDER = "gender";
+    public static final String SITE_ID = "siteId";
+    public static final String STAGE_ID = "stageId";
+
+    public static final String WITHADRAWAL_STUDY_DATE = "withadrawalStudyDate";
+    public static final String WITHADRAWAL_VAC_DATE = "withadrawalVacDate";
+
+    public static final String SCREENING_PLANNING_DATE = "screeningPlanningDate";
+    public static final String SCREENING_ACTUAL_DATE = "screeningActualDate";
+
+    public static final String PRIME_PLANNING_DATE = "primePlanningDate";
+    public static final String PRIME_ACTUAL_DATE = "primeActualDate";
+
+    public static final String PRIME_FFUP_ACTUAL_DATE = "primeFUPActualDate";;
+    public static final String PRIME_FFUP_PLANNING_DATE = "primePlanningDate";;
+
+    public static final String BOOST_PLANNING_DATE = "boostPlanningDate";
+    public static final String BOOST_ACTUAL_DATE = "boostActualDate";
+
+    public static final String BOOST1FU_PLANNING_DATE = "boost1FUAPlanningDate";
+    public static final String BOOST1FU_ACTUAL_DATE = "boost1FUActualDate";
+
+    public static final String BOOST2FU_ACTUAL_DATE = "boost2FUActualDate";
+    public static final String BOOST2FU_PLANNING_DATE = "boost2FUAPlanningDate";
+
+    public static final String BOOST3FU_ACTUAL_DATE = "boost3FUActualDate";
+    public static final String BOOST3FU_PLANNING_DATE = "boost3FUAPlanningDate";
+
+    public static final String FIRST_LTV_ACTUAL_DATE = "firstLTVActualDate";
+    public static final String FIRST_LTV_PLANNING_DATE = "firstLTVPlanningDate";
+
+    public static final String SEC_LTV_ACTUAL_DATE = "secLTVActualDate";
+    public static final String SEC_LTV_PLANNING_DATE = "secLTVPlanningDate";
+
+    public static final String THIRD_LTV_ACTUAL_DATE = "thirdLTVActualDate";
+    public static final String THIRD_LTV_PLANNING_DATE = "thirdLTVPlanningDate";
+
+    private static final int MIN_RANDOM_NUM = 11;
+    private static final int MAX_RANDOM_NUM = 99;
+
     private String serverURL;
     private String zetesRegistrationURL;
     private String fetchCsvUrl;
     private String importCsvUrl;
+    private String sCsvFile;
+    private Map<String, String> prop = new HashMap<String, String>() {
+
+        private static final long serialVersionUID = 1L;
+
+        {
+            put(SITE_ID, "B05-SL10001");
+            put(PARTICIPANT_ID, "9999999952");
+            put(DATE_OF_BIRTH, "1981-10-09");
+            put(GENDER, "M");
+            put(STAGE_ID, "2");
+
+            put(WITHADRAWAL_VAC_DATE, "");
+            put(WITHADRAWAL_STUDY_DATE, "");
+
+            put(SCREENING_ACTUAL_DATE, "2016-08-10");
+            put(SCREENING_PLANNING_DATE, "");
+
+            put(PRIME_ACTUAL_DATE, "");
+            put(PRIME_PLANNING_DATE, "2016-09-03");
+
+            put(PRIME_FFUP_ACTUAL_DATE, "");
+            put(PRIME_FFUP_PLANNING_DATE, "2016-09-03");
+
+            put(BOOST_ACTUAL_DATE, "");
+            put(BOOST_PLANNING_DATE, "2016-10-03");
+
+            put(BOOST1FU_ACTUAL_DATE, "");
+            put(BOOST1FU_PLANNING_DATE, "2017-01-03");
+
+            put(BOOST2FU_ACTUAL_DATE, "");
+            put(BOOST2FU_PLANNING_DATE, "2017-03-03");
+
+            put(BOOST3FU_ACTUAL_DATE, "");
+            put(BOOST3FU_PLANNING_DATE, "2017-03-03");
+
+            put(FIRST_LTV_ACTUAL_DATE, "");
+            put(FIRST_LTV_PLANNING_DATE, "2017-06-03");
+
+            put(SEC_LTV_ACTUAL_DATE, "");
+            put(SEC_LTV_PLANNING_DATE, "2017-09-03");
+
+            put(THIRD_LTV_ACTUAL_DATE, "");
+            put(THIRD_LTV_PLANNING_DATE, "2017-12-03");
+
+        }
+    };
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UITestHttpClientHelper.class);
+    private static final String URL_IMPORT_CSV = "/module/ebodac/web-api/import-csv";
+    private static final String URL_FETCH_CSV = "/module/ebodac/web-api/fetch-csv";
+    private static final String URL_PARTICIPANT_REGISTRATION = "/module/ebodac/registration/submit";
 
     public UITestHttpClientHelper(String serverUrl) {
         try {
@@ -22,6 +119,7 @@ public class UITestHttpClientHelper extends EbodacHttpClient {
                 this.setZetesRegistrationURL(this.getServerURL() + URL_PARTICIPANT_REGISTRATION);
                 this.setFetchCsvUrl(this.getServerURL() + URL_FETCH_CSV);
                 this.setImportCsvUrl(this.getServerURL() + URL_IMPORT_CSV);
+                this.setSCsvFile(this.generateCSVFileFromString());
 
             } else {
                 LOGGER.error("UITestHttpClientHelper -  serverUrl cannot build the constructor properly");
@@ -57,6 +155,42 @@ public class UITestHttpClientHelper extends EbodacHttpClient {
         }
 
         return status;
+    }
+
+    public boolean addVisits(String userName, String password, Map<String, String> properties, String csvFileString) {
+        boolean status = false;
+
+        try {
+            if (null != properties) {
+                this.updateProperties(properties);
+                this.setSCsvFile(this.generateCSVFileFromString());
+            } else {
+                setSCsvFile(csvFileString);
+            }
+
+            if (null != this.getSCsvFile()) {
+                // Define the right url to access to the IMPORT-CSV
+                this.sendCsvFile(this.getImportCsvUrl(), userName, password,
+                        new ByteArrayInputStream(this.getSCsvFile().getBytes("UTF-8")));
+                status = true;
+            } else {
+                status = false;
+            }
+        } catch (UnsupportedEncodingException e) {
+            status = false;
+            LOGGER.error("addVisits - UnsupportedEncodingException . Reason : " + e.getLocalizedMessage(), e);
+        } catch (NullPointerException e) {
+            status = false;
+            LOGGER.error("addVisits - NullPointerException . Reason : " + e.getLocalizedMessage(), e);
+
+        } catch (Exception e) {
+            status = false;
+            LOGGER.error("addVisits - Exception . Reason : " + e.getLocalizedMessage(), e);
+
+        }
+
+        return status;
+
     }
 
     public void fetchCSV(String userName, String password) {
@@ -103,6 +237,121 @@ public class UITestHttpClientHelper extends EbodacHttpClient {
         }
         this.serverURL = result;
 
+    }
+
+    public Map<String, String> getProp() {
+        return this.prop;
+    }
+
+    public void setProp(Map<String, String> prop) {
+        this.prop = prop;
+    }
+
+    public String getSCsvFile() {
+        return this.sCsvFile;
+    }
+
+    public void setSCsvFile(String sCsvFile) {
+        this.sCsvFile = sCsvFile;
+    }
+
+    private void updateProperties(Map<String, String> newProps) {
+        // We add the properties for specific key
+
+        Iterator<?> iter = newProps.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            this.getProp().put(entry.getKey().toString(), entry.getValue().toString());
+        }
+
+    }
+
+    private String generateCSVFileFromString() {
+        return "SiteNumber,Subject,BRTHDT,SEX,STAGE,PRMDT,BOOSTDT,VACDSDT,TRDSDT,VISIT,VISITDT,VISITDTPRJ\n"
+                + this.getProp().get(SITE_ID) + "," + this.getProp().get(PARTICIPANT_ID) + ","
+                + this.getProp().get(DATE_OF_BIRTH) + "," + this.getProp().get(GENDER) + ","
+                + this.getProp().get(STAGE_ID) + "," + this.getProp().get(PRIME_ACTUAL_DATE) + ","
+                + this.getProp().get(BOOST_ACTUAL_DATE) + "," + this.getProp().get(WITHADRAWAL_VAC_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_STUDY_DATE) + ",Screening," + this.getProp().get(SCREENING_ACTUAL_DATE)
+                + "," + this.getProp().get(SCREENING_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",Prime Vaccination Day," + this.getProp().get(PRIME_ACTUAL_DATE) + ","
+                + this.getProp().get(PRIME_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",Prime Vaccination First Follow-up visit," + this.getProp().get(PRIME_FFUP_ACTUAL_DATE) + ","
+                + this.getProp().get(PRIME_FFUP_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",Boost Vaccination Day," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(BOOST_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",Boost Vaccination First Follow-up visit," + this.getProp().get(BOOST1FU_ACTUAL_DATE) + ","
+                + this.getProp().get(BOOST1FU_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",Boost Vaccination Second Follow-up visit," + this.getProp().get(BOOST2FU_ACTUAL_DATE) + ","
+                + this.getProp().get(BOOST2FU_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",Boost Vaccination Third Follow-up visit," + this.getProp().get(BOOST3FU_ACTUAL_DATE) + ","
+                + this.getProp().get(BOOST3FU_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",First Long-term Follow-up visit," + this.getProp().get(FIRST_LTV_ACTUAL_DATE) + ","
+                + this.getProp().get(FIRST_LTV_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",Second Long-term Follow-up visit," + this.getProp().get(SEC_LTV_ACTUAL_DATE) + ","
+                + this.getProp().get(SEC_LTV_PLANNING_DATE) + "\n" + this.getProp().get(SITE_ID) + ","
+                + this.getProp().get(PARTICIPANT_ID) + "," + this.getProp().get(DATE_OF_BIRTH) + ","
+                + this.getProp().get(GENDER) + "," + this.getProp().get(STAGE_ID) + ","
+                + this.getProp().get(PRIME_ACTUAL_DATE) + "," + this.getProp().get(BOOST_ACTUAL_DATE) + ","
+                + this.getProp().get(WITHADRAWAL_VAC_DATE) + "," + this.getProp().get(WITHADRAWAL_STUDY_DATE)
+                + ",Third Long-term Follow-up visit," + this.getProp().get(THIRD_LTV_ACTUAL_DATE) + ","
+                + this.getProp().get(THIRD_LTV_PLANNING_DATE) + "\n";
+
+    }
+
+    public String generateNewParticipantId(String originalId) {
+        StringBuffer result = new StringBuffer();
+        try {
+
+            Integer newVal = new Integer(getRandomArbitary(MIN_RANDOM_NUM, MAX_RANDOM_NUM));
+            result.append(originalId.substring(0, originalId.length() - 2)).append(newVal);
+        } catch (NullPointerException e) {
+            LOGGER.error("generateNewParticipantId - NPE . Reason : " + e.getLocalizedMessage(), e);
+
+        } catch (Exception e) {
+            LOGGER.error("generateNewParticipantId - Exception . Reason : " + e.getLocalizedMessage(), e);
+
+        }
+        return result.toString();
+    }
+
+    /**
+     * Returns a random number between min and max
+     */
+    private int getRandomArbitary(int min, int max) {
+        return (int) (Math.round(Math.random() * (max - min) + min));
     }
 
 }
--- ebodac/src/test/java/org/motechproject/ebodac/uitest/test/GetVisitDataFromRAVEUiTest.java
+++ ebodac/src/test/java/org/motechproject/ebodac/uitest/test/GetVisitDataFromRAVEUiTest.java
@@ -1,8 +1,10 @@
 package org.motechproject.ebodac.uitest.test;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,24 +18,7 @@ import org.motechproject.ebodac.uitest.page.VisitPage;
 import static org.junit.Assert.assertTrue;
 
 public class GetVisitDataFromRAVEUiTest extends TestBase {
-    private static final String SERVICE_IMPORT_CSV = "/module/ebodac/web-api/import-csv";
-    private String stringCSVFile = "SiteNumber,Subject,BRTHDT,SEX,STAGE,PRMDT,BOOSTDT,VACDSDT,TRDSDT,VISIT,VISITDT,VISITDTPRJ\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Screening,2016-08-01,\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Prime Vaccination Day,2016-08-10,\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Prime Vaccination First Follow-up visit,,2016-10-15\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Boost Vaccination Day,,2016-12-03\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Boost Vaccination First Follow-up visit,,2016-12-10\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Boost Vaccination Second Follow-up visit,,2016-12-24\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Boost Vaccination Third Follow-up visit,,2017-03-11\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,First Long-term Follow-up visit,,2017-04-04\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Second Long-term Follow-up visit,,2017-06-03\n"
-            + "B05-SL10001,9999999952,1981-10-09,M,2,,,,2016-08-10,Third Long-term Follow-up visit,,2017-08-09\n";
-
-    private static final String PARTICIPANT_ID = "9999999952";
-    private static final String NEW_SCREENING_ACTUAL_DATE = "2016-08-01";
-    private static final int WAIT_500MLSEC = 500;
-    private static final long WAIT_2SEC = 2000;
-    private static final long WAIT_5SEC = 5000;
+    private Map<String, String> prop = new HashMap<String, String>();
     private LoginPage loginPage;
     private HomePage homePage;
     private EBODACPage ebodacPage;
@@ -41,6 +26,12 @@ public class GetVisitDataFromRAVEUiTest extends TestBase {
     private String user;
     private String password;
     private String url;
+    private String participantId = "9999999902";
+
+    private static final String NEW_SCREENING_ACTUAL_DATE = "2016-08-01";
+    private static final int WAIT_500MLSEC = 500;
+    private static final long WAIT_2SEC = 2000;
+    private static final long WAIT_5SEC = 5000;
     private static final String LOCAL_TEST_MACHINE = "localhost";
 
     private UITestHttpClientHelper httpClientHelper;
@@ -54,9 +45,11 @@ public class GetVisitDataFromRAVEUiTest extends TestBase {
             password = getTestProperties().getPassword();
             url = getServerUrl();
             httpClientHelper = new UITestHttpClientHelper(url);
+            // We change the participant id
+            setParticipantId(httpClientHelper.generateNewParticipantId(participantId));
             // We start the visits and the participants.
-            // initNewParticipantAndVisits();
-            initReplaceNewParticipant(PARTICIPANT_ID);
+            initReplaceNewParticipantFile(this.getParticipantId());
+            // We start the pages.
             loginPage = new LoginPage(getDriver());
             homePage = new HomePage(getDriver());
             ebodacPage = new EBODACPage(getDriver());
@@ -67,32 +60,34 @@ public class GetVisitDataFromRAVEUiTest extends TestBase {
             }
 
         } catch (NullPointerException e) {
-            getLogger().error("setup - NullPointerException . Reason : " + e.getLocalizedMessage(), e);
+            getLogger().error("setup - NPE . Reason : " + e.getLocalizedMessage(), e);
         } catch (Exception e) {
             getLogger().error("setup - Exception . Reason : " + e.getLocalizedMessage(), e);
         }
 
     }
 
-    public void initReplaceNewParticipant(String participantId) {
+    public void initReplaceNewParticipantFile(String participantId) {
 
         try {
             // Add a participant in the Participant.
             participant = new TestParticipant();
             participant.setId(participantId);
             participant.setParticipantId(participantId);
-
             if (httpClientHelper.addParticipant(participant, user, password)) {
                 // Add visits for the participant
-                InputStream in = new ByteArrayInputStream(stringCSVFile.getBytes("UTF-8"));
-                // Define the right url to access to the IMPORT-CSV
-                httpClientHelper.sendCsvFile((new StringBuffer(url)).append(SERVICE_IMPORT_CSV).toString(), user,
-                        password, in);
+                prop.put(UITestHttpClientHelper.PARTICIPANT_ID, participant.getParticipantId());
+                prop.put(UITestHttpClientHelper.DATE_OF_BIRTH, "1970-01-01");
+                prop.put(UITestHttpClientHelper.GENDER, "M");
+                prop.put(UITestHttpClientHelper.SCREENING_ACTUAL_DATE, NEW_SCREENING_ACTUAL_DATE);
+                prop.put(UITestHttpClientHelper.PRIME_ACTUAL_DATE,
+                        (new SimpleDateFormat("yyyy-MM-dd")).format(new Date()));
+                httpClientHelper.addVisits(user, password, prop, null);
             } else {
                 getLogger().error("setup - Cannot add the participant ");
             }
-        } catch (IOException e) {
-            getLogger().error("initNewParticipantAndVisits - IOException . Reason : " + e.getLocalizedMessage(), e);
+        } catch (Exception e) {
+            getLogger().error("initNewParticipantAndVisits - Exception . Reason : " + e.getLocalizedMessage(), e);
         }
     }
 
@@ -103,20 +98,15 @@ public class GetVisitDataFromRAVEUiTest extends TestBase {
             homePage.openEBODACModule();
             // We open the Ebodac page
             ebodacPage.goToPage();
-
-            // Validate New Participant
-            ebodacPage.goToPage();
-
-            if (ebodacPage.findByParticipantID(PARTICIPANT_ID)) {
-
-                ebodacPage.sleep(WAIT_500MLSEC);
+            ebodacPage.sleep(WAIT_500MLSEC);
+            if (ebodacPage.findByParticipantID(participantId)) {
                 // Go to Visit
                 visitPage = new VisitPage(getDriver());
                 // Go to Visit Page
                 ebodacPage.goToVisit();
                 visitPage.goToPage();
                 visitPage.sleep(WAIT_2SEC);
-                boolean selectParticipant = visitPage.findByParticipantId(PARTICIPANT_ID);
+                boolean selectParticipant = visitPage.findByParticipantId(participantId);
                 visitPage.sleep(WAIT_2SEC);
 
                 // Add assert to set that the participant is added and the
@@ -124,18 +114,16 @@ public class GetVisitDataFromRAVEUiTest extends TestBase {
                     // Visits select specific visit
                     assertTrue(visitPage.findActualDate(NEW_SCREENING_ACTUAL_DATE));
                 } else {
-                    getLogger().error("setDataVisitsTest - No participant Found with ID : " + PARTICIPANT_ID);
+                    getLogger().error("setDataVisitsTest - No participant Found with ID : " + participantId);
                 }
                 ebodacPage.sleep(WAIT_5SEC);
                 // visits.
 
             } else {
-                getLogger().error("setDataVisitsTest - No participant Found  with ID :" + PARTICIPANT_ID);
+                getLogger().error("setDataVisitsTest - No participant Found  with ID :" + participantId);
             }
 
-        } catch (
-
-        AssertionError e) {
+        } catch (AssertionError e) {
             getLogger().error("getVisitDataFromRAVETest - AssertionError . Reason : " + e.getLocalizedMessage(), e);
         } catch (InterruptedException e) {
             getLogger().error("getVisitDataFromRAVETest - NullPointerException . Reason : " + e.getLocalizedMessage(),
@@ -151,6 +139,14 @@ public class GetVisitDataFromRAVEUiTest extends TestBase {
     @After
     public void tearDown() throws Exception {
         logout();
+    }
+
+    public String getParticipantId() {
+        return participantId;
+    }
+
+    public void setParticipantId(String participantId) {
+        this.participantId = participantId;
     }
 
 }
